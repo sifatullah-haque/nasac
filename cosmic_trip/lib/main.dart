bdee9066d446237136c25c0995ae9aed5d9b1a55
sifatullah-haque <sifatulla.haque@gmail.com>
2023-09-28 11:29:56 +0600

    first page done

--- cosmic_trip/lib/main.dart
+++ cosmic_trip/lib/main.dart
@@ -1,125 +1,190 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ScreenUtilInit(
+      designSize: Size(414, 896),
+      minTextAdapt: true,
+      child: MaterialApp(
+        title: 'Material App',
+        home: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/background.png"),
+              fit: BoxFit.contain,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            child: Scaffold(
+                backgroundColor: Colors.transparent,
+                appBar: AppBar(
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                  leading: const Icon(
+                    Icons.menu_open_sharp,
+                    color: Colors.white,
+                    size: 40.0,
+                  ),
+                  centerTitle: true,
+                  title: Row(
+                    children: [
+                      Text(
+                        "Cosmic",
+                        style: GoogleFonts.roboto(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 35.sp,
+                        ),
+                      ),
+                      Text(
+                        "Trip",
+                        style: GoogleFonts.roboto(
+                          fontWeight: FontWeight.w200,
+                          fontSize: 35.sp,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                body: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                  child: Column(
+                    children: [
+                      SearchBar(),
+                      const Spacer(),
+                      PlanetImages(),
+                      PlanetaryTexts(),
+                      const Spacer(),
+                    ],
+                  ),
+                )),
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class PlanetaryTexts extends StatelessWidget {
+  const PlanetaryTexts({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Mercury",
+          style: GoogleFonts.roboto(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            fontSize: 45.sp,
+          ),
+        ),
+        Text(
+          "Planet",
+          style: GoogleFonts.roboto(
+            color: Colors.white,
+            fontSize: 45.sp,
+          ),
+        ),
+        SizedBox(
+          height: 10.h,
+        ),
+        Text(
+          "Mercury: The Sun-Kissed World of Mysteries and Scientific Marvels ",
+          style: GoogleFonts.roboto(
+            color: Colors.white,
+            fontSize: 15.sp,
+          ),
+        ),
+        Row(
+          children: [
+            TextButton(
+                onPressed: () {},
+                child: Row(
+                  children: [
+                    Text(
+                      "View More",
+                      style: GoogleFonts.roboto(
+                        fontSize: 18.sp,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Icon(
+                      Icons.arrow_right,
+                      color: Colors.white,
+                    ),
+                  ],
+                )),
           ],
         ),
+      ],
+    );
+  }
+}
+
+class PlanetImages extends StatelessWidget {
+  const PlanetImages({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset(
+        "assets/mercury.png",
+        width: 338.w,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class SearchBar extends StatelessWidget {
+  const SearchBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: SizedBox(
+        width: 350.w,
+        child: TextField(
+          decoration: InputDecoration(
+            prefixIcon: Icon(Icons.search),
+            prefixIconColor: Colors.white.withOpacity(0.8),
+            hintText: "Search your favorite planet",
+            hintStyle: GoogleFonts.roboto(),
+            fillColor: Colors.white.withOpacity(0.5),
+            filled: true,
+            contentPadding:
+                const EdgeInsets.symmetric(vertical: 15.0, horizontal: 08.0),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(24.r),
+              borderSide: const BorderSide(color: Colors.transparent, width: 0),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(24.r),
+              borderSide: const BorderSide(color: Colors.transparent, width: 0),
+            ),
+            focusedBorder: OutlineInputBorder(
+              gapPadding: 0.0,
+              borderRadius: BorderRadius.circular(24.r),
+              borderSide: const BorderSide(color: Colors.transparent, width: 0),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ cosmic_trip/lib/pages/constant/use_color.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+abstract class UseColor {
+  static const itsWhite = Color(0xffffffff);
+}
